9b8c4310ceffcd0617442cbadcde2c144891d3f0
erkki <erkki@192.168.0.105>
2016-12-04 12:52:11 +0200

    test

---
+++ DTEK1066/src/harkka.java
@@ -0,0 +1,13 @@
+/*
+ * Auth Ek1, ikiviita & justicecase 
+ */
+public class harkka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+		
+		
+	}
+
+}
